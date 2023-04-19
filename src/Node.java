bc20e4f6490d870cb12c86389e0d845b0fefc907
Elena <9513495670@mail.ru>
2023-04-19 16:51:57 +0300

    node

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    int value;
+    Node next;
+    Node previous;
+
+    public Node(int value, Node next, Node previous) {
+        this.value = value;
+        this.next = next;
+        this.previous = previous;
+    }
+}
